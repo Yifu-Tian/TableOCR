21836f6a775cb3037fb247203b456709c07b70f5
root <root@yifu>
2024-05-04 17:43:22 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
+  // basePath 和 assetPrefix 设置确保资源正确加载，当部署到非根目录时
   basePath: '/TableOCR',
-  assetPrefix: '/TableOCR',
+  assetPrefix: '/TableOCR/',
   output: "export",
 };
-
 module.exports = nextConfig;
 
