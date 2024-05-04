1b396748c88ed1af716218ce93a386caf98d3b46
root <root@yifu>
2024-05-04 22:57:53 +0800

    deploy

--- app/page.js
+++ app/page.js
@@ -16,7 +16,12 @@ export default function Home() {
       method: 'POST',
       body: formData
     })
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Server responded with an error: ' + res.status);
+      }
+      return res.json();
+    })
     .then(data => console.log("result", data))
     .catch(err => console.log(err))
     .finally(() => setUploading(false))
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+module.exports = {
+  // Use the basePath and assetPrefix settings to adjust the paths for GitHub Pages
+  basePath: '/TableOCR',
+  assetPrefix: '/TableOCR',
 
+  // Exporting as a static website
+  output: 'standalone',
+};
 module.exports = nextConfig
